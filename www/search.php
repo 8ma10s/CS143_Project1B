899f8b319abb40670d15ec18a6e5f0443d0e782b
Yamato Sasaki <yamatosasaki14@gmail.com>
2017-05-01 01:42:20 -0700

    Modify search.php to show Movie Information (single word search only)

--- www/search.php
+++ www/search.php
@@ -1,7 +1,9 @@
 <?php include 'header.php';?>
+<?php include 'database.php'; ?>
 
 <?php
 
+$db_connection = connectToDB();
 $validSearch = !empty($_GET['query']);
 
 if ($validSearch) {
@@ -10,7 +12,12 @@ if ($validSearch) {
 ?>
 
 <div class="row">
-  <?php if ($validSearch): ?>
+  <?php if ($validSearch):
+    // search movie
+    $movieQuery = "SELECT id, title, year
+    FROM Movie
+    WHERE title LIKE '%$query%'";
+    $movieResult = mysql_query($movieQuery, $db_connection); ?>
 
     <h2>Showing Results For "<?php echo $query; ?>"</h2>
     <h3>Movies</h3>
@@ -18,17 +25,27 @@ if ($validSearch) {
       <thead>
 	<tr>
 	  <th>Title</th>
+    <th>Year</th>
 	</tr>
       </thead>
       <tbody>
-	<tr>
-	  <td>
-	    <a href="./movie.php?id=1">Titanic</a>
-	  </td>
-	</tr>
+        <?php
+        // generate each row with Movie title and Year
+           while($row = mysql_fetch_row($movieResult)){
+             echo "<tr>";
+             echo "<td>";
+             echo "<a href=\"./movie.php?id=".$row[0]."\">".$row[1]."</a>";
+             echo "</td>";
+
+             echo "<td>";
+             echo $row[2];
+             echo "</td>";
+
+             echo "</tr>";
+           } ?>
       </tbody>
     </table>
-    
+
     <h3>Actors</h3>
     <table class="table table-hover">
       <thead>
@@ -52,4 +69,5 @@ if ($validSearch) {
   <?php endif; ?>
 </div>
 
+<?php mysql_close($db_connection); ?>
 <?php include 'footer.php' ?>
