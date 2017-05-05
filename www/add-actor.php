968c8aa9888c2a73c0d1eec2de8cd7597e780002
Yamato Sasaki <yamatosasaki14@gmail.com>
2017-05-05 00:42:10 -0700

    Fix error messages to show up in red and fix indentation

--- www/add-actor.php
+++ www/add-actor.php
@@ -2,87 +2,92 @@
 
 <div class="row">
   <h2>Add New Actor</h2>
-  <?php
-  //if not the first time you open the page
-  if(!empty($_GET)): ?>
-    <div class="alert alert-success alert-dismissible" role="alert">
-      <button type="button" class="close" data-dismiss="alert" ><span>&times;</span></button>
-      <?php
-      $iError= 0; //input error
-      $actorInfo = array(); //store all info about actor being added
-      // check that each necessary field is not empty. if empty, return error
-      if(empty($_GET['firstName'])){
-        echo "<strong>Error!</strong> First name cannot be empty.<br>";
+  <?php //if not the first time you open the page ?>
+  <?php if(!empty($_GET)) :?>
+    <?php
+    $iError= 0; //input error
+    $actorInfo = array(); //store all info about actor being added
+    $outStr = ""; //store all strings that need to be outputted at the end
+    // check that each necessary field is not empty. if empty, return error
+    if(empty($_GET['firstName'])){
+      $outStr = $outStr."<strong>Error!</strong> First name cannot be empty.<br>";
+      $iError=1;
+    }
+    $actorInfo['first'] = $_GET['firstName'];
+
+    if(empty($_GET['lastName'])){
+      $outStr = $outStr."<strong>Error!</strong> Last name cannot be empty.<br>";
+      $iError=1;
+    }
+    $actorInfo['last'] = $_GET['lastName'];
+
+    if(empty($_GET['sex'])){
+      $outStr = $outStr."<strong>Error!</strong> You must specify male/female.<br>";
+      $iError=1;
+    }
+    $actorInfo['sex'] = $_GET['sex'];
+
+    if(empty($_GET['dob'])){
+      $outStr = $outStr."<strong>Error!</strong> Date of birth cannot be empty.<br>";
+      $iError=1;
+    }
+
+    //dob must match format and be in the correct range
+    else if(!preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/",$_GET['dob'])){
+      $outStr = $outStr."<strong>Error!</strong> Date of birth must be in the format yyyy-mm-dd.<br>";
+      $iError=1;
+    }
+    else{
+      $actorInfo['dob'] = str_replace("-","",$_GET['dob']);
+      //separate year, month, and day component of the string dob
+      $yr = substr($actorInfo['dob'], 0, 4);
+      $mo = substr($actorInfo['dob'],4,2);
+      $da = substr($actorInfo['dob'],6,2);
+      if(!checkdate($mo,$da,$yr)){
+        $outStr = $outStr."<strong>Error!</strong> Invalid date of birth range.<br>";
         $iError=1;
       }
-      $actorInfo['first'] = $_GET['firstName'];
+    }
 
-      if(empty($_GET['lastName'])){
-        echo "<strong>Error!</strong> Last name cannot be empty.<br>";
-        $iError=1;
-      }
-      $actorInfo['last'] = $_GET['lastName'];
-
-      if(empty($_GET['sex'])){
-        echo "<strong>Error!</strong> You must specify male/female.<br>";
-        $iError=1;
-      }
-      $actorInfo['sex'] = $_GET['sex'];
-
-      if(empty($_GET['dob'])){
-        echo "<strong>Error!</strong> Date of birth cannot be empty.<br>";
-        $iError=1;
-      }
-
-      //dob must match format and be in the correct range
-      else if(!preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/",$_GET['dob'])){
-        echo "<strong>Error!</strong> Date of birth must be in the format yyyy-mm-dd.<br>";
+    //if dod is empty, null should be inserted
+    if(empty($_GET['dod'])){
+      $actorInfo['dod'] = '\\N';
+    }
+    //if nonempty, dod must match format and be in the correct range
+    else{
+      if(!preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/",$_GET['dod'])){
+        $outStr = $outStr."<strong>Error!</strong> Date of death must be in the format yyyy-mm-dd.<br>";
         $iError=1;
       }
       else{
-        $actorInfo['dob'] = str_replace("-","",$_GET['dob']);
-        $yr = substr($actorInfo['dob'], 0, 4);
-        $mo = substr($actorInfo['dob'],4,2);
-        $da = substr($actorInfo['dob'],6,2);
+        $actorInfo['dod'] = str_replace("-","",$_GET['dod']);
+        //separate year, month, and day component of the string dod
+        $yr = substr($actorInfo['dod'], 0, 4);
+        $mo = substr($actorInfo['dod'],4,2);
+        $da = substr($actorInfo['dod'],6,2);
         if(!checkdate($mo,$da,$yr)){
-          echo "<strong>Error!</strong> Invalid date of birth range.<br>";
+          $outStr = $outStr."<strong>Error!</strong> Invalid date of death range.<br>";
+          $iError=1;
+        }
+        //if correct, dod must be before dob
+        else if($actorInfo['dod'] < $actorInfo['dob']){
+          $outStr = $outStr."<strong>Error!</strong> Date of death must be same as or after date of birth.<br>";
           $iError=1;
         }
       }
-
-      //if dod is empty, null should be inserted
-      if(empty($_GET['dod'])){
-        $actorInfo['dod'] = '\\N';
-      }
-      //if nonempty, dod must match format and be in the correct range
-      else{
-        if(!preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/",$_GET['dod'])){
-          echo "<strong>Error!</strong> Date of death must be in the format yyyy-mm-dd.<br>";
-          $iError=1;
-        }
-        else{
-          $actorInfo['dod'] = str_replace("-","",$_GET['dod']);
-          $yr = substr($actorInfo['dod'], 0, 4);
-          $mo = substr($actorInfo['dod'],4,2);
-          $da = substr($actorInfo['dod'],6,2);
-          if(!checkdate($mo,$da,$yr)){
-            echo "<strong>Error!</strong> Invalid date of death range.<br>";
-            $iError=1;
-          }
-          //if correct, dod must be before dob
-          else if($actorInfo['dod'] < $actorInfo['dob']){
-            echo "<strong>Error!</strong> Date of death must be same as or after date of birth.<br>";
-            $iError=1;
-          }
-        }
-      }
-
-      //if no error, success
-      if($iError==0){
-        echo "<strong>Success!</strong> Actor added to database.<br>";
-      }
-      ?>
-    </div>
+    }
+    //if no error, success ?>
+    <?php if($iError==0) :?>
+      <div class="alert alert-success alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" ><span>&times;</span></button>
+        <strong>Success!</strong> Actor added to database.<br>
+      </div>
+    <?php else :?>
+      <div class="alert alert-danger alert-dismissible" role="alert">
+        <button type="button" class="close" data-dismiss="alert" ><span>&times;</span></button>
+        <?php echo $outStr; ?>
+      </div>
+    <?php endif; ?>
   <?php endif; ?>
 
   <form method="GET" action="#">
