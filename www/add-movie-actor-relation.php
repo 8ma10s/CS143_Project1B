c3e7af487afa647457a66d22c3cce7e101e4fc91
Ben Lawson <blinternetgeek@gmail.com>
2017-05-05 20:43:51 -0700

    finished movie/actor relation, removed fancy search from actors since the list is huge

--- www/add-movie-actor-relation.php
+++ www/add-movie-actor-relation.php
@@ -1,26 +1,67 @@
+<?php include 'database.php'; ?>
+
+<?php
+
+$db_connection = connectToDB();
+
+if (!empty($_POST['mid'])) {
+  $isValid = true;
+
+  $mid = $_POST['mid'];
+  $aid = $_POST['aid'];
+  $role = $_POST['role'];
+
+  $relationQuery = "INSERT INTO MovieActor (mid, aid, role) 
+VALUES ('$mid', '$aid', '$role')";
+
+  $relationResult = mysql_query($relationQuery, $db_connection);
+  if (!$relationResult) {
+    $isValid = false;
+  }
+}
+else {
+  $isValid = false;
+}
+
+// get all movies
+$movieQuery = "SELECT id, title FROM Movie";
+$movieResults = mysql_query($movieQuery, $db_connection);
+
+// get all actors
+$actorQuery = "SELECT id, first, last FROM Actor";
+$actorResults = mysql_query($actorQuery, $db_connection);
+
+?>
+
 <?php include 'header.php';?>
 
 <div class="row">
   <h2>Add Actor/Movie Relation</h2>
-  <?php if (!empty($_GET['movie'])): ?>
+  <?php if ($isValid): ?>
     <div class="alert alert-success alert-dismissible" role="alert">
       <button type="button" class="close" data-dismiss="alert" ><span>&times;</span></button>
-      <strong>Success!</strong> Movie/Actor relationship added to database.
+      <strong>Success!</strong> Actor/Movie relationship added to database.
     </div>
   <?php endif; ?>
-  <form method="GET" action="#">
+  <form method="POST" action="#">
     <div class="form-group">
       <label for="movie">Movie</label>
-      <select name="movie" class="fancy-dropdown form-control">
-	<option value="1">Titanic</option>
-	<option value="2">Avatar</option>
+      <select name="mid" class="fancy-dropdown form-control">
+	<?php
+	while ($row = mysql_fetch_assoc($movieResults)) {
+	  echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+	}
+	?>
       </select>
     </div>
     <div class="form-group">
       <label for="actor">Actor</label>
-      <select name="actor" class="fancy-dropdown form-control">
-	<option value="1">Johnny Depp</option>
-	<option value="2">Brad Pitt</option>
+      <select name="aid" class="form-control">
+	<?php
+	while ($row = mysql_fetch_assoc($actorResults)) {
+	  echo '<option value="'.$row['id'].'">'.$row['first'].' '.$row['last'].'</option>';
+	}
+	?>
       </select>
     </div>
     <div class="form-group">
