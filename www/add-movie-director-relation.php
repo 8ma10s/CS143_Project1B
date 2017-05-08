5d3e38b7aabf1d4d5bb2debfa0d5fcbc07159e5d
Ben Lawson <blinternetgeek@gmail.com>
2017-05-08 12:57:28 -0700

    finished add movie/director relation

--- www/add-movie-director-relation.php
+++ www/add-movie-director-relation.php
@@ -1,26 +1,66 @@
+<?php include 'database.php'; ?>
+
+<?php
+
+$db_connection = connectToDB();
+
+if (!empty($_POST['mid'])) {
+  $isValid = true;
+
+  $mid = $_POST['mid'];
+  $did = $_POST['did'];
+
+  $relationQuery = "INSERT INTO MovieDirector (mid, did) 
+VALUES ('$mid', '$did')";
+
+  $relationResult = mysql_query($relationQuery, $db_connection);
+  if (!$relationResult) {
+    $isValid = false;
+  }
+}
+else {
+  $isValid = false;
+}
+
+// get all movies
+$movieQuery = "SELECT id, title FROM Movie";
+$movieResults = mysql_query($movieQuery, $db_connection);
+
+// get all directors
+$directorQuery = "SELECT id, first, last FROM Director";
+$directorResults = mysql_query($directorQuery, $db_connection);
+
+?>
+
 <?php include 'header.php';?>
 
 <div class="row">
   <h2>Add Actor/Movie Relation</h2>
-  <?php if (!empty($_GET['movie'])): ?>
+  <?php if ($isValid): ?>
     <div class="alert alert-success alert-dismissible" role="alert">
       <button type="button" class="close" data-dismiss="alert" ><span>&times;</span></button>
       <strong>Success!</strong> Movie/Director relation added to database.
     </div>
   <?php endif; ?>
-  <form method="GET" action="#">
+  <form method="POST" action="#">
     <div class="form-group">
-      <label for="movie">Movie</label>
-      <select name="movie" class="fancy-dropdown form-control">
-	<option value="1">Titanic</option>
-	<option value="2">Avatar</option>
+      <label for="mid">Movie</label>
+      <select name="mid" class="fancy-dropdown form-control">
+	<?php
+	while ($row = mysql_fetch_assoc($movieResults)) {
+	  echo '<option value="'.$row['id'].'">'.$row['title'].'</option>';
+	}
+	?>
       </select>
     </div>
     <div class="form-group">
-      <label for="director">Director</label>
-      <select name="director" class="fancy-dropdown form-control">
-	<option value="1">Johnny Depp</option>
-	<option value="2">Brad Pitt</option>
+      <label for="did">Director</label>
+      <select name="did" class="fancy-dropdown form-control">
+	<?php
+	while ($row = mysql_fetch_assoc($directorResults)) {
+	  echo '<option value="'.$row['id'].'">'.$row['first'].' '.$row['last'].'</option>';
+	}
+	?>
       </select>
     </div>
     <button type="submit" class="btn btn-default">Submit</button>
@@ -35,4 +75,4 @@
  })
 </script>
 
-<?php include 'footer.php' ?>
+<?php include 'footer.php'; ?>
